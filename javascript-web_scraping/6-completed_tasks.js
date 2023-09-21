b3001bce093598f09b8dff670883b48e2a6481e7
solp22 <sol.puentereal@gmail.com>
2023-09-21 13:20:13 -0700

    change dict

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -8,9 +8,11 @@ request(urlToRequest, function (error, response, body) {
     console.error('error:', error);
   }
   const data = JSON.parse(body);
-  tasksCompleted = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0,
-   '6': 0, '7': 0, '8': 0, '9': 0, '10': 0};
+  tasksCompleted = {};
   data.forEach(task => {
+    if (!tasksCompleted[task.userId]) {
+      tasksCompleted[task.userId] = 0;
+    }
     if (task.completed === true ){
       tasksCompleted[task.userId] += 1;
     }
