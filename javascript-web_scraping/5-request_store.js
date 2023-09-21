8bcb9a0d267ce80953ae888e6277a8aae8a127b7
solp22 <sol.puentereal@gmail.com>
2023-09-21 12:56:03 -0700

    task 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const urlToRequest = process.argv[2];
+const filePath = process.argv[3];
+
+request(urlToRequest, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  }
+  fs.appendFile(filePath, body, 'utf-8', function (err) {
+    if (err) {
+      console.error(err);
+    }
+  })
+});
