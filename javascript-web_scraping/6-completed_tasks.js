cf9777376aaf61664ed92ec33af1f79751c4b03c
solp22 <sol.puentereal@gmail.com>
2023-09-21 13:17:37 -0700

    task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const urlToRequest = process.argv[2];
+
+request(urlToRequest, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  }
+  const data = JSON.parse(body);
+  tasksCompleted = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0,
+   '6': 0, '7': 0, '8': 0, '9': 0, '10': 0}
+  data.forEach(task => {
+    if (task.completed === true ){
+      tasksCompleted[task.userId] += 1;
+    }
+  });
+  console.log(tasksCompleted);
+});
