90ae1c1a46972d41d2cc5cdc7de57733964043d9
solp22 <sol.puentereal@gmail.com>
2023-09-21 11:58:05 -0700

    semistandard

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,8 @@ const request = require('request');
 const urlToRequest = process.argv[2];
 
 request(urlToRequest, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  }
   console.log('code:', response.statusCode);
 });
